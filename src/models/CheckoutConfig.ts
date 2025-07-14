424fdf36b1cfbbe85a1ebc9a3251e19c39fe054a
saif <seif@kamelizer.com>
2025-07-14 20:26:56 +0300

    Refactor CheckoutConfig interface: update to use Item and IPromocodeConfig, streamline properties for delivery fees and discounts, and remove unused fields for improved clarity.

--- src/models/CheckoutConfig.ts
+++ src/models/CheckoutConfig.ts
@@ -1,32 +1,15 @@
-import { CartItem } from './CartItem';
-import { OfferItem } from './OfferItem';
+import { Item } from '../engines/items';
+import { IPromocodeConfig } from '../engines/promocode';
 
-export interface UserPointsInfo {
-  points: number;
-  pendingPoints: number;
-  pointsValue: number;
-  pendingPointsValue: number;
-}
+
 
 export interface CheckoutConfig {
-  menuItems: CartItem[];
-  offers: OfferItem[];
-  deliveryType: 1 | 2 | 3;
-  branchConfig: {
-    branch_delivery_charge?: number;
-    streets_delivery_time?: boolean;
-    delivery_time?: number;
-  };
-  addressConfig?: {
-    street: {
-      delivery_charge: number;
-      delivery_time: number;
-    };
-  };
-  promocodeFixed?: number;
-  promocodePercentage?: number;
-  deliveryPromocode?: boolean;
-  dineinFixed?: number;
-  dineinPercentage?: number;
-  userPointsInfo?: UserPointsInfo;
+  menuItems?: Item[];
+  offers?: Item[];
+  deliveryFeesEgp?:number;
+  loyaltyDiscount?:number;
+  promoCodeDiscount?:number;
+  dineinPercentage?:number;
+  dineinFixed?:number;
+  coupon?:IPromocodeConfig
 } 
